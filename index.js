3e315696ab93f70ae4384f6a71552db59e708e96
Egor <yatskovskiy2001@gmail.com>
2021-11-03 01:24:20 +0300

    Доработаны действия в скобках , добавлены комментарии, убраны некотрые баги

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let { number: lastNumber} = this.findLastNumber(symbols);
                 let lastIndex = symbols.length - 1;
 
-                // если символ не проходит проверки - не выодим его и не добавляем в массив
+                // если символ не проходит проверки - не выводим его и не добавляем в массив
                 function checkSymbol (symbol) {
                     let bollean = false;
                     // Настройка для корректного показа "."
@@ -77,6 +77,25 @@ document.addEventListener("DOMContentLoaded", () => {
                 str+=symbol;
                 input.value = str;
             },
+            checkStaples () {
+                let check = 0;
+                let rightStaple = 0;
+                let leftStaple = 0;
+                for (let  i = 0; i < symbols.length; i++) {
+                    let item = symbols[i];
+                    if ( item === '(') {
+                        check++;
+                        rightStaple++;
+                    } else if ( item === ')') {
+                        check--;
+                        leftStaple++;
+                    }
+                    if (leftStaple > rightStaple) {
+                        throw new Error(" Закрывающих скобок больше открывающих");
+                    }
+                }
+                return check === 0 ? true : false;
+            },
             //Вычисления
             start(arr) {
             // Вычисления будут производится в соответствии с массивом приоритета выполнения действий
@@ -91,6 +110,8 @@ document.addEventListener("DOMContentLoaded", () => {
                         } else if ( item === ')') {
                             morePriority-=100;
                         }
+                        // если перед минусом или плюсом есть ( , то она не будет внесена в массив
+                        if ( (item === '-' || item === '+') && arr[i - 1] === '(' ) { continue; }
                         switch(item) {
                             case "+": chars.push( {name: "+", index: i,priority: 1 + morePriority,} );
                             break;
@@ -114,14 +135,25 @@ document.addEventListener("DOMContentLoaded", () => {
                     let firstExpressionIndex = null;
                     let lastExpressionIndex = null;
                     let count = 1;
-
+                    // находим а
                     for ( let i = index-1; i >= 0; i--) {
-                        if ( !arrFromDisplay[i].match(/[\d.]/) ) break;
+                        // если перед числом есть знак выражнние ([-+] то выполняем и выходим
+                        // нужно для того, чтобы в число а входил + или -
+                        if ( arrFromDisplay[i].match(/[-+]/) && arrFromDisplay[i-1] === '(') {
+                            firstExpressionIndex = i;
+                            count++;
+                            a.push(arrFromDisplay[i]);
+                            break;
+                        }
+
+                        if ( !arrFromDisplay[i].match(/[\d.]/) ) {
+                            break;
+                        }
                         firstExpressionIndex = i;
                         count++;
                         a.push(arrFromDisplay[i]);
                     }
-                    
+                    // находим b
                     for ( let i = index+1; i < arrFromDisplay.length; i++) {
                         if ( !arrFromDisplay[i].match(/[\d.]/) ) break;
                         lastExpressionIndex = i;
@@ -143,14 +175,15 @@ document.addEventListener("DOMContentLoaded", () => {
                         case "*": total = calcMethod.multiplication(a,b);
                             break;
                     }
+                    //вместо предыдущих 2ух слагаемых подставляем тотал 
+                    //(если числа были заключнены в скобки, то скобки тоже убираем)
                     if (newArr[firstExpressionIndex-1] === '(' && newArr[lastExpressionIndex+1] === ')') {
                         newArr.splice(firstExpressionIndex - 1,count + 2,`${total}`);
                     } else {
                         newArr.splice(firstExpressionIndex,count,`${total}`);
                     }
-                    //вместо предыдущих 2ух слагаемых подставляем тотал
                 }
-                // Убирает скобки у числа заключенного в  них, меняет arr
+                // Убирает скобки у числа заключенного в  них , например (321) = 321
                 function changeNumInStaples () {
                     let regexp = /\((\d+)\)/g;
                     let matchAll = str.matchAll(regexp);
@@ -169,23 +202,26 @@ document.addEventListener("DOMContentLoaded", () => {
                         lessIndex -= 2;
                     }
                 }
-
-                if ( str.match(/\(\d+\)/) ) {
-                    changeNumInStaples();
-                }
+                // если не все скобки закрыты , то не продолжаем
+                if ( !this.checkStaples() ) return false;
+                // если есть выражение вида "[-+/*] )" - выходим
+                if ( str.match(/[-+/*]\)/)) return false;
                 // если в массиве нету хотя бы 2ух слагаемых или последний элемент массива - знак
                 if ( !arr.join('').match(/\-?\.?\d+\.?[+*/-]\.?\(?\d+\)?/) || arr[arr.length - 1].match(/[-/+*]/)) return false;
-                //changeNumInStaples()
-                    let newArr = arr.slice();
-                    // Вычисляем значение выражения из дисплея
-                    while (true) {
-                        let sortChars = makePriority(newArr);
-                        if( sortChars.length === 0) break;  
-                        result(newArr,sortChars[0]);
-                        sortChars.splice(0,1);
-                    }
-                    // Если true, можем прожать знак равно (выполнить функцию calcMethod.equally())
-                    return true;
+
+                // если есть число , заключенное в скобки - убираем скобки
+                if ( str.match(/\(\d+\)/) ) { changeNumInStaples();}
+                
+                let newArr = arr.slice();
+                // Вычисляем значение выражения
+                while (true) {
+                    let sortChars = makePriority(newArr);
+                    if( sortChars.length === 0) break;  
+                    result(newArr,sortChars[0]);
+                    //sortChars.splice(0,1);
+                }
+                // Если true, можем прожать знак равно (выполнить функцию calcMethod.equally())
+                return true;
             },
             //находим последнее число в массиве
             findLastNumber (arr) {
@@ -201,7 +237,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     }
                     a = a.reverse().join('');
                 }
-                // возвращаем число,его индекс м исходном массиве и длину
+                // возвращаем число,его индекс в исходном массиве и длину
                 return {
                     number: a,
                     index: firstIndex,
@@ -211,24 +247,28 @@ document.addEventListener("DOMContentLoaded", () => {
             // Здесь выводим выражение в output
             showInOutput() {
                 let totalStr = String(calcMethod.total);
-                // если есть 2 слагаемых - выводим результат 
-                if (symbols.join('').match(/-?\d+\.?[-+/*]\d+\.?/g)) {
+                let boolean = true;
+                // ожидаем закрытие скобок
+                if ( !this.checkStaples()) { boolean = false; }
+                // если есть выражение вида "[-+/*] )" или последний символ - знак
+                if ( str.match(/[-+/*]\)/) || str[str.length - 1].match(/[-+/*]/)) { boolean = false; }
+                // если есть 2 слагаемых - выводим результат
+                if (boolean && symbols.join('').match(/-?\d+\.?[-+/*]\d+\.?/)) {
                     output.textContent = `${symbols.join('')} = ${totalStr}`;
-
-                if ( calcMethod.total > 100000) {
-                    console.log("!!!!");
-                    totalStr = `${totalStr[0]},${totalStr.slice(2,5)}*10^${totalStr.slice(1,-1).length + 1}`;
-                }
-
-                if(str.length > 12) {
-                    if (calcMethod.total > Math.pow(10,14)) {
-                        console.log(Math.pow(10,14))
-                        output.textContent = "bigNumber";
-                    } else {
-                        output.textContent = `${symbols.slice(0,10).join('')}... = ${totalStr}`;
+                    
+                    if ( calcMethod.total > 100000) {
+                        totalStr = `${totalStr[0]},${totalStr.slice(2,5)}*10^${totalStr.slice(1,-1).length + 1}`;
                     }
-                }
-                } else  {
+
+                    if(str.length > 12) {
+                        if (calcMethod.total > Math.pow(10,14)) {
+                            console.log(Math.pow(10,14))
+                            output.textContent = "bigNumber";
+                        } else {
+                            output.textContent = `${symbols.slice(0,10).join('')}... = ${totalStr}`;
+                        }
+                    }
+                } else {
                     output.textContent = '';
                 }
 
@@ -251,11 +291,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     throw new ValidationError("Недопустимые символы");
                 } else if ( str.match(/[+/*-]\.[+/*-]/)) {
                     throw new ValidationError("Лишние знаки");
-                }       
+                } else if (str.match(/\d+\(/)) {
+                    throw new ValidationError("Перед скобкой нет знака действия");
+                } else if ( str.match(/\(\)/)) {
+                    throw new ValidationError("Пустые скобки");
+                } else if ( str.match(/\([/*]/)) {
+                    throw new ValidationError("Недопустимые знаки после скобки");
+                }
             },
+            // меняем шрифт у input
             changeFont(str) {
                 if (str.length >= 26) {
-                    input.style.fontSize = "14px";
+                    input.style.fontSize = "15px";
                 } else if (str.length >= 18) {
                     input.style.fontSize = "20px";
                 } else if (str.length >= 15) {
@@ -281,7 +328,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 input.value = str;
             },
             percent() {
-                let lastIndex = str.length - 1
+                // делим последнее число на 100 (если проходим проверку)
+                let lastIndex = str.length - 1;
                 if (str[lastIndex].match(/[-+/*]/) || (str.match(/[-+/*]\./) && str[lastIndex] === '.') || (str[0] === '.' && str.length === 1)) return;
                 let {number,index,length} = calcFunc.findLastNumber(symbols);
                 number = `${+(number/100).toFixed(5)}`;
@@ -307,10 +355,6 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             equally() {
                 if (this.success) {
-                    if(this.total > Math.pow(10,14)) {
-                        this.total = 0;
-                        output.textContent = "Слишком большое число";
-                    }
                     input.value = this.total;
                     symbols = `${this.total}`.split('').slice();
                     str = `${this.total}`;
@@ -398,22 +442,32 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             }
             calcFunc.changeFont(str);
-            calcFunc.isError(str);
-            calcMethod.success = calcFunc.start(symbols);
-            calcFunc.showInOutput();
+            try {
+                // Проверяем ошибки
+                calcFunc.isError(str);
+                // запуск основной функции
+                calcMethod.success = calcFunc.start(symbols);
+                // вывод в output
+                calcFunc.showInOutput();
+            } catch(err) {
+                output.textContent = "Ошибка";
+                console.log(err.message);
+            }
         });
 
         input.addEventListener( 'input', () => {
             calcFunc.changeFont(str);
             try {
+                // получаем массив из input
                 symbols = input.value.split('');
+                // получаем строку
                 str = symbols.join('');
                 calcFunc.isError(str);
                 calcMethod.success = calcFunc.start(symbols);
                 calcFunc.showInOutput();
             } catch (err) {
                 output.textContent = "Ошибка";
-                console.log(err.message)
+                console.log(err.message);
             }
         });
     };
